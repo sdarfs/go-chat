90c1306ea72bd9501ad5c66fd1eb94c22fe75714
Daria <darya.funck@yandex.ru>
2024-05-25 13:07:20 +0700

    fix network

--- server/router/router.go
+++ server/router/router.go
@@ -15,13 +15,13 @@ func InitRouter(userHandler *user.Handler, wsHandler *ws.Handler) {
 	r = gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"*"}, //{"http://localhost:3000"},
 		AllowMethods:     []string{"GET", "POST"},
 		AllowHeaders:     []string{"Content-Type"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		AllowOriginFunc: func(origin string) bool {
-			return origin == "http://localhost:3000"
+			return true
 		},
 		MaxAge: 12 * time.Hour,
 	}))
